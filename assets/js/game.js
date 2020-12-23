b19ac74327ddc6646f9221fb45ab17488c4412a5
symlar <40181569+symlara@users.noreply.github.com>
2020-12-22 18:30:10 -0600

    added for and while loop to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,9 +12,11 @@ console.log(playerName, playerAttack, playerHealth);
 
 
 
+
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+        // Alert players that they are starting the round
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -69,6 +71,13 @@ var fight = function(enemyName) {
     };
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-}
+
+
+    }
+    
+    for (var i = 0; i < enemyNames.length; i++) {
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+ 
