5f048df5350bae34eb4f627d4a48d00560f3ddae
symlar <40181569+symlara@users.noreply.github.com>
2020-12-22 20:24:19 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,8 +70,18 @@ var fight = function(enemyName) {
   };
     
     for (var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over");
+            break;
+        }
         var pickedEnemyName = enemyNames[i];
         enemyHealth = 50;
+        // use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
         fight(pickedEnemyName);
     }
 
