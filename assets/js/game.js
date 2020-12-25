d58d60358980c57412bda59727706bc7864391b3
symlar <40181569+symlara@users.noreply.github.com>
2020-12-24 22:29:38 -0600

    created a function getPlayerName() that will require the user to input a player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,12 +74,14 @@ var fight = function(enemy) {
 
     // other logic remains the same
 
-    debugger;
+    
     for (var i = 0; i < enemyInfo.length; i++) {
+      
         // if player is still alive, keep fighting
         if (playerInfo.health > 0) {
           // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
           window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+          
       
           // pick new enemy to fight based on the index of the enemy.name array
           var pickedEnemyObj = enemyInfo[i];
@@ -179,8 +181,19 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
